e8fe040b548717e5bfef172b37f244c07f9fd534
2024RishabhT <9960646@bedfordnhk12.net>
2022-03-30 17:39:25 -0400

    Adding missing SmartDashboard calls

--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -3,22 +3,14 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 import edu.wpi.first.wpilibj.DigitalInput;
-<<<<<<< HEAD
-=======
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
->>>>>>> origin/thinkpad
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Indexer extends SubsystemBase {
   private final WPI_TalonFX motor = new WPI_TalonFX(9, Constants.CANIVORE);
-<<<<<<< HEAD
   private final DigitalInput bottomInput = new DigitalInput(0);
   private final DigitalInput topInput = new DigitalInput(1);
-=======
-  private final DigitalInput im_a_bottom = new DigitalInput(0);
-  private final DigitalInput im_a_top = new DigitalInput(1);
->>>>>>> origin/thinkpad
 
   public Indexer() {
     motor.setInverted(false);
@@ -27,7 +19,8 @@ public class Indexer extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-<<<<<<< HEAD
+    SmartDashboard.putBoolean("bottom", bottomInput.get());
+    SmartDashboard.putBoolean("top", topInput.get());
     if (topInput.get()) {
       if (!bottomInput.get())
         moveTheThing(0.1d);
@@ -36,19 +29,6 @@ public class Indexer extends SubsystemBase {
     }
     else
       moveTheThing(0.0d);
-=======
-    SmartDashboard.putBoolean("bottom", im_a_bottom.get());
-    SmartDashboard.putBoolean("top", im_a_top.get());
-    if (im_a_top.get()){
-      if(!im_a_bottom.get()){
-        moveTheThing(0.1);
-      } else {
-        moveTheThing(0);
-      }
-    } else {
-      moveTheThing(0);
-    }
->>>>>>> origin/thinkpad
   }
 
   @Override
